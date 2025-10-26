90e2d263a975d30e50b16ab89710ae9aecb8c0bb
nagamani1824 <nagamani1824@gmail.com>
2025-10-26 19:02:06 +0530

    Implement Calculator class with basic operations

--- Problem-1.js
+++ Problem-1.js
@@ -1,1 +1,26 @@
+"
+class Calculator {
+    constructor(a, b) {
+        this.a = parseFloat(a);
+        this.b = parseFloat(b);
+    }
 
+    calculate(operation) {
+        switch(operation) {
+            case "add":
+            case "+":
+                return this.a + this.b;
+            case "subtract":
+            case "-":
+                return this.a - this.b;
+            case "multiply":
+            case "*":
+                return this.a * this.b;
+            case "divide":
+            case "/":
+                return this.b !== 0 ? this.a / this.b : "Cannot divide by zero";
+            default:
+                return "Invalid Operation";
+        }
+    }
+}
