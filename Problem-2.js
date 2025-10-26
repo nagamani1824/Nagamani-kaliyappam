c1795184e881c13928514805a973c6dee1fa4a52
Nagamani <nagamani1824@gmail.com>
2025-10-26 22:30:12 +0530

    Update Problem-2.js

--- Problem-2.js
+++ Problem-2.js
@@ -1,22 +1,4 @@
-/**
- * Problem-2:
- * With a single integer as the input, generate the following until a = x
- *
- * Output examples:
- *  input a = 1 -> 1
- *  input a = 2 -> 1, 3
- *  input a = 3 -> 1, 3, 5
- *  input a = 4 -> 1, 3, 5, 7
- *
- * Interpretation:
- * Print the first `a` odd numbers starting from 1.
- *
- * Usage:
- *  node Problem-2.js <a>
- * Example:
- *  node Problem-2.js 4
- *  -> 1,3,5,7
- */
+
 
 function firstNOdds(a) {
   a = Number(a);
